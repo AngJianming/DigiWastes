87dddd81203ab33cb2198b8525a5ffe05dfc8768
AngJianming <angjianming2005@hotmail.com>
2025-04-23 14:06:41 +0800

    update latest recharts for admin dashboard

---
+++ src/Components/AreaChart.jsx
@@ -0,0 +1,15 @@
+// adding 'use client' at the top so that we can see inactivity to the chart
+'use client';
+
+import { AreaChart, Area, ResponsiveContainer } from "recharts";
+
+const productSales = [
+    
+]
+
+const AreaChartComponent = () => {
+    return <h1>Area Chart</h1>;
+
+};
+
+export default AreaChartComponent;
